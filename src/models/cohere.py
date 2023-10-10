ca55e9014a1e4f2c932601bf18afb31797e91ad1
Dinuja Wattage <dinuja.w9@gmail.com>
2023-10-09 21:24:59 -0400

    added error handlign via dataclasses, made a model class, made a cohere class too

--- src/models/cohere.py
+++ src/models/cohere.py
@@ -1,21 +1,50 @@
 import cohere
-import os
+from models.base import BaseChatbot, CompletionResponse
+from utils.enums import BaseEnum
 
-from dotenv import load_dotenv
-load_dotenv()
 
-api_key = os.getenv('API_KEY')
+@dataclass
+class CompletionResponse:
+    payload: str
+    message: str
+    err: str
 
-co = cohere.Client(api_key)
+class CohereModel(str, BaseEnum)
+    COMMAND = "command"
+    COMMAND_LIGHT = "command-light"
+    COMMAND_NIGHTLY = "command-nightly"
+    COMMAND_LIGHT_NIGHTLY = "command-light-nightly"
 
-def cohere_ai_completion(prompt):
-    response = co.chat(
-    # chat_history is not a needed parameter
-    chat_history=[
-        {"role": "USER", "message": "Who discovered gravity?"},
-        {"role": "CHATBOT", "message": "The man who is widely credited with discovering gravity is Sir Isaac Newton"}
-    ],
-    message=prompt,
-    connectors=[{"id": "web-search"}] # perform web search before answering the question
-    )
-    return response.text
+
+class Cohere(BaseChatBot)
+    def __init__(
+        self,
+        message: str = "",
+        model: CohereModel = CohereModel.COMMAND,
+        temperature: float = 0,
+        api_key: str = "",
+    ) -> None:
+        self.message = message,
+        self.model = model,
+        self.temperature = temperature,
+        Cohere.api_key = api_key
+    
+    def cohere_ai_completion(self) -> CompletionResponse:
+        try:
+            co = cohere.Client(Cohere.api_key)
+            response = co.chat(
+            message=prompt,
+            connectors=[{"id": "web-search"}] # perform web search before answering the question
+            )
+        except cohere.CohereError as e:
+            return CompletionResponse(
+                payload="",
+                message="FAILED",
+                err=e.message,
+            )
+        return CompletionResponse(
+            payload=response.text,
+            message="OK",
+            err="",
+        )
+    
