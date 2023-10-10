c94064cf94f5e55a22843ead78cf10f63842d50e
Dinuja Wattage <dinuja.w9@gmail.com>
2023-10-10 18:03:42 -0400

    added in cohere test cases

--- test.py
+++ test.py
@@ -3,8 +3,8 @@ from llmproxy import llmproxy
 
 def main() -> None:
     prompt = "What is 1+1?"
-    print(llmproxy.get_completion(prompt=prompt))
-
+    print("OPEN AI: %s", llmproxy.get_completion(prompt=prompt))
+    print("COHERE AI: %s", llmproxy.get_completion_cohere(prompt=prompt))
 
 if __name__ == "__main__":
     main()
