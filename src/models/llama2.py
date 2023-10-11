49f636f251624f0c7ea3cd625a43838f3fc63b72
DesNahback <79017770+DesNahback@users.noreply.github.com>
2023-10-11 06:39:52 -0500

    adding error handling

--- src/models/llama2.py
+++ src/models/llama2.py
@@ -9,30 +9,33 @@ class Llama2(BaseChatbot):
         system_prompt: str = None,
         api_key: str = "",
         ) -> None:
-        self.prompt = prompt
         self.system_prompt = system_prompt or "Answer politely"
+        self.prompt = prompt
         self.api_key = api_key
-    
-    def get_completion(self) -> CompletionResponse:
         API_URL = "https://api-inference.huggingface.co/models/meta-llama/Llama-2-7b-chat-hf"
         headers = {"Authorization": f"Bearer {self.api_key}"}
+    def get_completion(self) -> CompletionResponse:
+        if self.prompt is "":
+            return self._handle_error(
+                exception="No prompt detected",error_type="InputError"
+            )
         try:
             def query(payload):
                  response = requests.post(API_URL, headers=headers, json=payload)
                  return response.json()
-            
             # Llama2 prompt template
             prompt_template = f"<s>[INST] <<SYS>>\n{{{{ {self.system_prompt} }}}}\n<</SYS>>\n{{{{ {self.prompt} }}}}\n[/INST]"
-
             payload = {
                 "inputs": prompt_template
             }
             output = query(payload)
-            
 
         except Exception as e:
             raise Exception("Error Occur")
         
         return CompletionResponse(
-            payload=output
+            payload = output
         )
+    
+    def _handle_error(self, exception: str, error_type: str) -> CompletionResponse:
+        return CompletionResponse(message=exception, err=error_type)
