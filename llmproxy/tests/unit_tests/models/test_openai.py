f17170b80a2702c146b1c4700fe985d49ae2a0ca
Ahmed Ali <ahmedomaraali@gmail.com>
2023-10-13 01:23:01 -0400

    Openai tests for unsupported model, invalid API key, and invalid Temprature

--- llmproxy/tests/unit_tests/models/test_openai.py
+++ llmproxy/tests/unit_tests/models/test_openai.py
@@ -1,14 +1,29 @@
-from enum import Enum, EnumMeta
+import os
+from llmproxy.models.openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+openai_api_key = os.getenv("OPENAI_API_KEY")
+
+def test_invalid_temperature():
+    chatbot = OpenAI(api_key = openai_api_key, temp=-1)
+    response = chatbot.get_completion()
+    assert isinstance(response.err, str)
+    assert "temperature" in response.message.lower()
+
+def test_invalid_api_key():
+    chatbot = OpenAI(api_key="invalid_key")
+    response = chatbot.get_completion()
+    assert isinstance(response.err, str)
+    assert "authentication" in response.message.lower()
+
+def test_unsupported_model():
+    chatbot = OpenAI(api_key=openai_api_key, model="unsupported_model")
+    response = chatbot.get_completion()
+    assert response.err == "ValueError"
+    assert response.message == "Model not supported"
 
 
-class MetaEnum(EnumMeta):
-    def __contains__(cls, item):
-        try:
-            cls(item)
-        except ValueError:
-            return False
-        return True
 
 
-class BaseEnum(Enum, metaclass=MetaEnum):
-    pass
