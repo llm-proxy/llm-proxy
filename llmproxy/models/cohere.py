503e23223fe5401e2c1fbe3350126569c7ce3e36
Dinuja Wattage <dinuja.w9@gmail.com>
2023-10-29 18:24:09 -0400

    added requested changes on PR

--- llmproxy/models/cohere.py
+++ llmproxy/models/cohere.py
@@ -11,15 +11,17 @@ class CohereModel(str, BaseEnum):
 class Cohere(BaseChatbot):
     def __init__(
         self,
-        message: str = "",
+        prompt: str = "",
         model: CohereModel = CohereModel.COMMAND,
         temperature: float = 0,
         api_key: str = "",
+        max_token: int = 0,
     ) -> None:
-        self.message = message
+        self.prompt = prompt
         self.model = model
         self.temperature = temperature
         self.api_key = api_key
+        self.max_token = max_token
         self.co = None
         try: 
             self.co = cohere.Client(self.api_key)
@@ -31,15 +33,20 @@ class Cohere(BaseChatbot):
            )  
 
     def get_completion(self) -> CompletionResponse:
+        if self.model not in CohereModel:
+            return self._handle_error(
+                exception=f"Model not supported. Please use one of the following models: {', '.join(Cohere.list_values())}",
+                error_type="ValueError",
+            )
         if self.co is None:
            return self.error_response
         try:
             response = self.co.chat(
-            message = self.message,
-            model = self.model,
-            connectors = [{"id": "web-search"}], # perform web search before answering the question
-            temperature = self.temperature            
-            )
+                max_tokens = self.max_token, 
+                message = self.prompt,
+                model = self.model,
+                temperature = self.temperature            
+                )
             return CompletionResponse(
                 payload=response.text,
                 message="OK",
@@ -52,5 +59,5 @@ class Cohere(BaseChatbot):
                 err=e.http_status
             )
         except Exception as e:
-            raise Exception("unknown error")
+            raise Exception("Unknown Cohere error when making API call")
     
