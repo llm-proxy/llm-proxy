187c322aca4b0613ecd7660a61f22c7e67500b4d
Dinuja Wattage <dinuja.w9@gmail.com>
2023-10-29 16:16:21 -0400

    added nltk import

--- llmproxy/routing/classification.py
+++ llmproxy/routing/classification.py
@@ -3,15 +3,28 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import classification_report
+from nltk.corpus import stopwords
+import re
+
+# Define a custom tokenizer that replaces numbers with "NUM" and removes stop words
+stop_words = set(stopwords.words('english'))
+
+def custom_tokenizer(text):
+    tokens = re.findall(r'\b\w+\b', text.lower())  # This pattern matches words (alphanumeric or underscore)
+    tokens = ['NUM' if token.isdigit() else token for token in tokens if token.lower() not in stop_words]  # Replace digits with "NUM" and remove stop words
+    return tokens
 
 # Load the CSV data into a DataFrame
-data = pd.read_csv('text_data.csv')
+data = pd.read_csv('prompt_data_set.csv')
 
 # Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(data['prompts'], data['category'], test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(data['prompt'], data['category'], test_size=0.2, random_state=42)
 
 # Create TF-IDF vectors
-tfidf_vectorizer = TfidfVectorizer(max_features=5000)
+tfidf_vectorizer = TfidfVectorizer(
+    tokenizer=custom_tokenizer,
+    lowercase=False  # We are converting to lowercase in the custom tokenizer                            
+)
 X_train_tfidf = tfidf_vectorizer.fit_transform(X_train)
 
 # Train a Multinomial Naive Bayes classifier
