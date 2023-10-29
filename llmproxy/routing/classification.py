ada2239926969d8bc5a2e6d0f4c88beeac5d8fb0
Dinuja Wattage <dinuja.w9@gmail.com>
2023-10-29 17:33:19 -0400

    updates small things

--- llmproxy/routing/classification.py
+++ llmproxy/routing/classification.py
@@ -4,46 +4,57 @@ from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import classification_report
 import re
-from nltk.corpus import stopwords
+import nltk
 
-# Define a custom tokenizer that replaces numbers with "NUM" and removes stop words
-stop_words = set(stopwords.words('english'))
+# Download the stopwords dataset if not already available
+try:
+    from nltk.corpus import stopwords
+    stopwords.words('english')
+except LookupError:
+    nltk.download('stopwords', quiet=True)
+    from nltk.corpus import stopwords
 
-def custom_tokenizer(text):
-    tokens = re.findall(r'\b\w+\b', text.lower())  # This pattern matches words (alphanumeric or underscore)
-    tokens = ['NUM' if token.isdigit() else token for token in tokens if token.lower() not in stop_words]  # Replace digits with "NUM" and remove stop words
-    return tokens
+class ClassificationModel():
+    def __init__(self) -> None:
+        stop_words = set(stopwords.words('english'))        
+       
+        # Define a custom tokenizer that replaces numbers with "NUM" and removes stop words
+        def custom_tokenizer(text):
+            tokens = re.findall(r'\b\w+\b', text.lower())  # This pattern matches words (alphanumeric or underscore)
+            tokens = ['NUM' if token.isdigit() else token for token in tokens if token.lower() not in stop_words]  # Replace digits with "NUM" and remove stop words
+            return tokens
 
-# Load the CSV data into a DataFrame
-data = pd.read_csv('prompt_data_set.csv')
+        # Load the CSV data into a DataFrame
+        data = pd.read_csv('prompt_data_set.csv')
 
-# Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(data['prompt'], data['category'], test_size=0.2, random_state=42)
+        # Split the data into training and testing sets
+        X_train, self.X_test, y_train, self.y_test = train_test_split(data['prompt'], data['category'], test_size=0.2, random_state=42)
 
-# Create TF-IDF vectors
-tfidf_vectorizer = TfidfVectorizer(
-    token_pattern=None,
-    tokenizer=custom_tokenizer,
-    lowercase=False  # We are converting to lowercase in the custom tokenizer                            
-)
+        # Create TF-IDF vectors
+        self.tfidf_vectorizer = TfidfVectorizer(
+            token_pattern=None,
+            tokenizer=custom_tokenizer,
+            lowercase=False  # We are converting to lowercase in the custom tokenizer                            
+        )
 
-X_train_tfidf = tfidf_vectorizer.fit_transform(X_train)
+        X_train_tfidf = self.tfidf_vectorizer.fit_transform(X_train)
 
-# Train a Multinomial Naive Bayes classifier
-nb_classifier = MultinomialNB()
-nb_classifier.fit(X_train_tfidf, y_train)
+        # Train a Multinomial Naive Bayes classifier
+        self.nb_classifier = MultinomialNB()
+        self.nb_classifier.fit(X_train_tfidf, y_train)
 
-# Function to classify user input
-def classify_input(user_input):
-    # Preprocess the user input
-    user_input_tfidf = tfidf_vectorizer.transform([user_input])
+    # Function to classify user input
+    def classify_input(self, user_input):
+        # Preprocess the user input
+        user_input_tfidf = self.tfidf_vectorizer.transform([user_input])
 
-    # Predict the category
-    category = nb_classifier.predict(user_input_tfidf)[0]
-    return category
+        # Predict the category
+        category = self.nb_classifier.predict(user_input_tfidf)[0]
+        return category
 
 if __name__ == "__main__":
+    model = ClassificationModel()
     # Evaluate the model
-    X_test_tfidf = tfidf_vectorizer.transform(X_test)
-    y_pred = nb_classifier.predict(X_test_tfidf)
-    print(classification_report(y_test, y_pred))
+    X_test_tfidf = model.tfidf_vectorizer.transform(model.X_test)
+    y_pred = model.nb_classifier.predict(X_test_tfidf)
+    print(classification_report(model.y_test, y_pred))
--- llmproxy/tests/unit_tests/routing/test_classification.py
+++ llmproxy/tests/unit_tests/routing/test_classification.py
@@ -1,6 +1,8 @@
-from llmproxy.routing.classification import classify_input
+from llmproxy.routing.classification import ClassificationModel
 
 def test_classification_science() -> None:
     prompt = "what is a cell made from?"
-    output = classify_input(prompt)
-    assert output == "Science"
+    model = ClassificationModel()
+    output = model.classify_input(prompt)
+    print(f"{output} == Science")
+        
