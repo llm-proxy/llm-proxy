6136f77a389dc4595103ec3a363355908d4bff78
Dinuja Wattage <dinuja.w9@gmail.com>
2023-10-29 18:04:03 -0400

    comments added

--- llmproxy/tests/unit_tests/routing/test_classification.py
+++ llmproxy/tests/unit_tests/routing/test_classification.py
@@ -1,8 +1,13 @@
 from llmproxy.routing.classification import ClassificationModel
 
 def test_classification_science() -> None:
+    # Arrange 
     prompt = "what is a cell made from?"
     model = ClassificationModel()
+    
+    # Act 
     output = model.classify_input(prompt)
-    print(f"{output} == Science")
+    
+    # Assert
+    assert output == "Science"
         
