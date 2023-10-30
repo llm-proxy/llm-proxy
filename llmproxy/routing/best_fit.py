85a1c6d9c5103e060123205c009780166734b1e0
laxitshahi <laxitshahi@laxits-mbp.home>
2023-10-29 22:18:34 -0400

    test

--- llmproxy/routing/best_fit.py
+++ llmproxy/routing/best_fit.py
@@ -1,7 +1,8 @@
 from transformers import pipeline
 
+
 def get_best_category(prompt: str, candidate_labels: list[str]) -> str:
-    classifier = pipeline(task="zero-shot-classification", model="facebook/bart-large-mnli")
+    classifier = pipeline(task="zero-shot-classification", model="facebook/bart-small")
     res = classifier(prompt, candidate_labels=candidate_labels)
 
     best_category = res["labels"][res["scores"].index(max(res["scores"]))]
@@ -9,12 +10,21 @@ def get_best_category(prompt: str, candidate_labels: list[str]) -> str:
 
 
 # Testing
-candidate_labels=["business", "math", "code to language", "language to code", "code generation"]
+candidate_labels = [
+    "business",
+    "math",
+    "code to language",
+    "language to code",
+    "code generation",
+]
 prompt = "Please tell me, what are the top 10 stocks"
-_, best_category = get_best_category(prompt=prompt, candidate_labels=candidate_labels)
+res, best_category = get_best_category(prompt=prompt, candidate_labels=candidate_labels)
+print(res)
 print(best_category)
 
-prompt = "I want to write python code that converts even numbers to odd and multiplies by 2"
+prompt = (
+    "I want to write python code that converts even numbers to odd and multiplies by 2"
+)
 res, best_category = get_best_category(prompt=prompt, candidate_labels=candidate_labels)
 print(res)
 print(best_category)
