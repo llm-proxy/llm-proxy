74725379fbee9b8a874d32ebd80241bb7e74b543
DesNahback <79017770+DesNahback@users.noreply.github.com>
2023-11-09 23:00:22 -0500

    Fix/63/config_yml issue #63

--- llmproxy/tests/unit_tests/config_yml/test_llmproxy.py
+++ llmproxy/tests/unit_tests/config_yml/test_llmproxy.py
@@ -9,19 +9,29 @@ from dotenv import load_dotenv
 
 load_dotenv(".env.test")
 
+script_dir = os.path.dirname(os.path.abspath(__file__))
 
-def empty_model_test() -> None:
+
+def get_absolute_path(file_name):
+    return os.path.join(script_dir, file_name)
+
+
+def test_empty_model() -> None:
     with pytest.raises(Exception, match="No models provided in user_settings"):
-        settings = _get_settings_from_yml(path_to_yml="empty_model_test.yml")
+        settings = _get_settings_from_yml(
+            path_to_yml=get_absolute_path("empty_model_test.yml")
+        )
         available_model = _setup_available_models(settings=settings)
         user_model = _setup_user_models(
             settings=settings, available_models=available_model
         )
 
 
-def invalid_model_test() -> None:
+def test_invalid_model() -> None:
     with pytest.raises(Exception, match="test is not a valid model"):
-        settings = _get_settings_from_yml(path_to_yml="invalid_model_test.yml")
+        settings = _get_settings_from_yml(
+            path_to_yml=get_absolute_path("invalid_model_test.yml")
+        )
         available_model = _setup_available_models(settings=settings)
         user_model = _setup_user_models(
             settings=settings, available_models=available_model
