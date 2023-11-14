5db33ff970208ec392bed9cdb76f62912a0dccbb
Dinuja Wattage <dinuja.w9@gmail.com>
2023-11-13 22:32:58 -0500

    changed approach of storing api key

--- llmproxy/routing/classification_two.py
+++ llmproxy/routing/classification_two.py
@@ -1,5 +1,5 @@
 # Import OpenAI's GPT 3.5 Turbo 
-from llmproxy.models.openai import OpenAI
+import openai
 import os
 from dotenv import load_dotenv
 
@@ -9,15 +9,15 @@ openai_api_key = os.getenv("OPENAI_API_KEY")
 
 def classify_prompt(prompt:str)->str:
     
-    client = OpenAI(api_key=openai_api_key)
+    openai.api_key=openai_api_key
 
-    response = client.chat.completions.create(
+    response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
             {"role": "system", "content": """You are a prompt classification assistant who will classify the user content 
-            into the following categories : Code Generation Task, Text Generation Task, Translation and Multilingual 
-            Applications Task, Natural Language Processing Task, Conversational AI Task, Educational Applications 
-            Task, Healthcare and Medical Task, Legal Task, Financial Task, Content Recommendation Task"""},
+            into the following categories and would ouput only the category name, no other text: Code Generation Task, Text 
+            Generation Task, Translation and Multilingual Applications Task, Natural Language Processing Task, Conversational
+            AI Task, Educational Applications Task, Healthcare and Medical Task, Legal Task, Financial Task, Content Recommendation Task"""},
             {"role": "user", "content": prompt},
         ]
     )
