40ea4881fae573f23c9a649dbe69d2a13570e9b7
Dinuja Wattage <dinuja.w9@gmail.com>
2023-11-17 16:09:33 -0500

    updated code for model

--- llmproxy/training/train_category_model.py
+++ llmproxy/training/train_category_model.py
@@ -1,34 +1,64 @@
 from datasets import load_dataset
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, TrainingArguments, Trainer
+import torch
 import numpy as np
 import evaluate
 
-dataset = load_dataset('csv', data_files={'train': 'train.csv', 'test': 'test.csv'})
-tokenizer = AutoTokenizer.from_pretrained('distilbert-base-uncased')
+print("loading dataset")
+dataset = load_dataset("csv", data_files={"train": "train.csv", "test": "test.csv"})
+tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
+print("dataset loaded")
 
 # Preprocess the dataset to be model readable
 def tokenization(batch):
-    return tokenizer(batch['text'], truncation=True, padding=True)
+    inputs = tokenizer(batch["prompt"], truncation=True, padding=True)
+    inputs["labels"] = batch["category"]
+    return inputs
 
 metric = evaluate.load("accuracy")
 
 # To measure accuracy of model
-def compute_metrics(eval_pred):
-    logits, labels = eval_pred
+def compute_metrics(p):
+    logits, labels = p.predictions, p.label_ids
     predictions = np.argmax(logits, axis=-1)
     return metric.compute(predictions=predictions, references=labels)
 
 encoded_dataset = dataset.map(tokenization, batched=True)
-classification_model = AutoModelForSequenceClassification.from_pretrained('distilbert-based-uncased', num_labels=10)
-training_args = TrainingArguments('test_trainer', evaluation_strategy="epoch")
+classification_model = AutoModelForSequenceClassification.from_pretrained("distilbert-base-uncased", num_labels=10)
+training_args = TrainingArguments("test_trainer", evaluation_strategy="epoch")
 
 trainer = Trainer(
     model=classification_model, 
     args=training_args, 
     train_dataset=encoded_dataset["train"], 
     eval_dataset=encoded_dataset["test"],
-    compute_metrics=compute_metrics
+    compute_metrics=compute_metrics,
 )
 
+# Custom training step
+def training_step(model, inputs):
+    # Print the keys of the first training example
+    if isinstance(inputs, dict):
+        print("Keys of the first training example:", inputs.keys())
+    
+    labels = inputs.pop("category", None)  # Try to get "category" key, default to None if not present
+    if labels is not None:
+        inputs["labels"] = labels
+    outputs = model(**inputs)
+    
+    # Check if the loss is not None
+    if outputs.loss is not None:
+        loss = outputs.loss
+    else:
+        loss = torch.tensor(0.0, device=outputs.logits.device)
+    
+    return loss
+
+trainer.training_step = training_step
+
+print("TRAINING MODEL...")
 trainer.train()
+print("TRAINING DONE!")
+print("SAVING MODEL...")
 classification_model.save_pretrained("llmproxy/data/classification")
+print("MODEL SAVED!")
