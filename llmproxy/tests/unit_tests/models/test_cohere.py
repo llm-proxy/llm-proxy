df9c44a5b691fb31b06f8fc8d73ce0b9922cc797
Dinuja Wattage <dinuja.w9@gmail.com>
2023-11-18 23:16:54 -0500

    finished unit test for cohere

--- llmproxy/tests/unit_tests/models/test_cohere.py
+++ llmproxy/tests/unit_tests/models/test_cohere.py
@@ -1,11 +1,11 @@
 import os
 from dotenv import load_dotenv
-from llmproxy.models.cohere import Cohere
+from llmproxy.models.cohere import Cohere, CohereModel
 
 load_dotenv(".env.test")
 
-cohere_api_key = os.getenv("COHERE_API_KEY")
-
+#cohere_api_key = os.getenv("COHERE_API_KEY")
+cohere_api_key = "iqAm7dJ9TEOwxFSetAfkurVyPjFLwlyQVo0Xd5oe"
 
 # TODO: May be a FLAKY test; Ensure this is not the case
 def test_get_estimated_max_cost():
@@ -20,7 +20,6 @@ def test_get_estimated_max_cost():
     actual_cost = cohere.get_estimated_max_cost(prompt=prompt)
     assert actual_cost == estimated_cost, "NOTE: Flaky test may need to be changed/removed in future based on pricing"
 
-
 def test_cohere_empty_api_key() -> None:
     # Arrange
     empty_api_key = ""
@@ -28,34 +27,61 @@ def test_cohere_empty_api_key() -> None:
     cohere_llm = Cohere(api_key=empty_api_key)
     # Assert
     assert(cohere_llm.error_response.err == "ValueError")
+    
+def test_cohere_invalid_api_key() -> None:
+    # Arrange
+    fake_api_key = "I am a fake api key"
+    prompt = "whats 1+1?"
+    # Act
+    cohere_llm = Cohere(api_key=fake_api_key)
+    response = cohere_llm.get_completion(prompt)
+    # Assert
+    assert(response.message == "invalid api token")
 
 def test_cohere_invalid_model() -> None:
     # Arrange
-    pass
+    cohere_model = "fake model"
+    cohere_llm = Cohere(model=cohere_model)
     # Act
-
+    response = cohere_llm.get_completion()
     # Assert
+    assert(response.message == f"Model not supported. Please use one of the following models: {', '.join(CohereModel.list_values())}")
 
 def test_cohere_negative_max_token() -> None:
-    pass
     # Arrange
-    
+    num_tokens = -100
+    cohere_llm = Cohere(api_key=cohere_api_key, max_output_tokens=num_tokens)
     # Act
-
+    response = cohere_llm.get_completion()
     # Assert
+    assert(response.message == "invalid request: message must be at least 1 token long.")
 
 def test_cohere_negative_temperature() -> None:
-    pass
     # Arrange
-    
+    prompt = "whats 1+1?"
+    negative_temp = -1
+    cohere_llm = Cohere(prompt=prompt, api_key=cohere_api_key, max_output_tokens=1000, temperature=negative_temp)
     # Act
-
+    response = cohere_llm.get_completion()
     # Assert
+    assert(response.message == "invalid request: temperature must be between 0 and 5 inclusive.")
 
 def test_cohere_temperature_above_five() -> None:
-    pass
     # Arrange
-    
+    prompt = "whats 1+1?"
+    temperature = 6
+    cohere_llm = Cohere(prompt=prompt, api_key=cohere_api_key, max_output_tokens=1000, temperature=temperature)
     # Act
-
+    response = cohere_llm.get_completion()
     # Assert
+    assert(response.message == "invalid request: temperature must be between 0 and 5 inclusive.")
+
+def test_cohere_success() -> None:
+    # Arrange
+    prompt = "whats 1+1?"
+    temperature = 0
+    cohere_llm = Cohere(prompt=prompt, api_key=cohere_api_key, max_output_tokens=1000, temperature=temperature)
+    # Act
+    response = cohere_llm.get_completion()
+    # Assert
+    assert(response.message == "OK")
