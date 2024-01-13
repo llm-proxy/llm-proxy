ca15776e3c3f5be267a81eefb84b614101840573
Dinuja Wattage <dinuja.w9@gmail.com>
2024-01-13 15:53:23 -0500

    moved categorize_text def up in code

--- llmproxy/llmproxy.py
+++ llmproxy/llmproxy.py
@@ -102,6 +102,28 @@ def _setup_user_models(available_models={}, settings={}) -> Dict[str, object]:
         raise e
 
 
+def categorize_text(prompt: str) -> str:
+    model = "facebook/bart-large-mnli"
+    candidate_labels = [
+        "Code Generation Task",
+        "Text Generation Task",
+        "Translation and Multilingual Applications Task",
+        "Natural Language Processing Task",
+        "Conversational AI Task",
+        "Educational Applications Task",
+        "Healthcare and Medical Task",
+        "Legal Task",
+        "Financial Task",
+        "Content Recommendation Task",
+    ]
+    logger.info(msg="Classification model is classifying the user prompt")
+    classifier = pipeline(task="zero-shot-classification", model=model)
+    logger.info(msg="The prompt has been classified\n")
+    results = classifier(prompt, candidate_labels)
+    best_category = results["labels"][0]
+    return best_category
+
+
 class LLMProxy:
     def __init__(
         self,
@@ -236,25 +258,3 @@ class LLMProxy:
                 "Requests to all models failed! Please check your configuration!"
             )
         return completion_res
-
-
-def categorize_text(prompt: str) -> str:
-    model = "facebook/bart-large-mnli"
-    candidate_labels = [
-        "Code Generation Task",
-        "Text Generation Task",
-        "Translation and Multilingual Applications Task",
-        "Natural Language Processing Task",
-        "Conversational AI Task",
-        "Educational Applications Task",
-        "Healthcare and Medical Task",
-        "Legal Task",
-        "Financial Task",
-        "Content Recommendation Task",
-    ]
-    logger.info(msg="Classification model is classifying the user prompt")
-    classifier = pipeline(task="zero-shot-classification", model=model)
-    logger.info(msg="The prompt has been classified\n")
-    results = classifier(prompt, candidate_labels)
-    best_category = results["labels"][0]
-    return best_category
