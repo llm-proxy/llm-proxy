5399cbabf9da45755d00903b5f4698981a86cb72
Alain Ballen <alainballen@gmail.com>
2024-01-16 21:50:35 -0500

    split test file into routing types

---
+++ test_cost.py
@@ -0,0 +1,17 @@
+from llmproxy.llmproxy import LLMProxy
+
+"""Temp Test file, will be removed in future in favour of unit/integration tests"""
+
+
+def main() -> None:
+    # prompt = "I am a man, not a man, but not a man, that is an apple, or a banana!"
+    prompt = "what is 1+1?"
+    # output = proxy_client.route(route_type="cost", prompt=prompt)
+    # print(output)
+    proxy_client = LLMProxy(path_to_configuration="api_configuration.yml")
+    output = proxy_client.route(route_type="cost", prompt=prompt)
+    print(output.payload)
+
+
+if __name__ == "__main__":
+    main()
