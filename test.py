9b989846ce5f4ddd5a2bd9aab55950b37fdc9a84
laxitshahi <laxitshahi@laxits-mbp.home>
2024-01-18 03:22:01 -0500

    small comment fix

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from llmproxy.llmproxy import LLMProxy
 
-"""Temp Test file, will be removed in future in favour of unit/integration tests"""
+# NOTE: Temp Test file, will be removed in future in favour of unit/integration tests
 
 
 def main() -> None:
@@ -12,7 +12,7 @@ def main() -> None:
     proxy_client = LLMProxy(
         path_to_user_configuration="llmproxy.config.yml",
     )
-    output = proxy_client.route(route_type="category", prompt=prompt)
+    output = proxy_client.route(route_type="cost", prompt=prompt)
     print(output.response)
     print(output.errors)
 
