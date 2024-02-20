609adf34ee7e29de31de6b3ae9948c90a4020573
Dinuja Wattage <dinuja.w9@gmail.com>
2024-02-20 00:09:44 -0500

    changed expected cost

--- llmproxy/tests/unit_tests/models/test_openai.py
+++ llmproxy/tests/unit_tests/models/test_openai.py
@@ -35,7 +35,7 @@ def test_get_estimated_max_cost():
     gpt = OpenAIAdapter(
         api_key=openai_api_key, max_output_tokens=256, model="gpt-3.5-turbo-1106"
     )
-    expected_cost = 0.000108
+    expected_cost = 0.00052
     prompt = "I am a cat in a hat!"
     price_data = {"prompt": 1e-06, "completion": 2e-06}
     # Act
