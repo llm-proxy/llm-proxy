9799da2dc17459558f1e650f1ed2f56501ce1c56
Alain Ballen <alainballen@gmail.com>
2024-03-04 17:10:46 -0500

    code cleanup

--- proxyllm/provider/google/vertexai.py
+++ proxyllm/provider/google/vertexai.py
@@ -5,6 +5,7 @@ from proxyllm.provider.base import BaseAdapter
 from proxyllm.utils import logger, timeout_function, tokenizer
 from proxyllm.utils.exceptions.provider import VertexAIException
 
+from llmproxy.utils.logger import CustomLogger, console_logger, file_logger
 
 # Dictionary mapping Vertex AI model categories to task performance ratings.
 vertexai_category_data = {
@@ -34,16 +35,16 @@ vertexai_category_data = {
             "Content Recommendation Task": 1,
         },
         "gemini-pro": {
-            "Code Generation Task": 3,
-            "Text Generation Task": 3,
-            "Translation and Multilingual Applications Task": 3,
-            "Natural Language Processing Task": 3,
-            "Conversational AI Task": 3,
-            "Educational Applications Task": 3,
-            "Healthcare and Medical Task": 3,
-            "Legal Task": 3,
-            "Financial Task": 3,
-            "Content Recommendation Task": 3,
+            "Code Generation Task": 1,
+            "Text Generation Task": 1,
+            "Translation and Multilingual Applications Task": 1,
+            "Natural Language Processing Task": 1,
+            "Conversational AI Task": 1,
+            "Educational Applications Task": 1,
+            "Healthcare and Medical Task": 2,
+            "Legal Task": 2,
+            "Financial Task": 2,
+            "Content Recommendation Task": 1,
         },
     }
 }
