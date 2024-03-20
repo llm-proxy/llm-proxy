7ef7d5f222e122985745ac355bea35996668d4b0
Glitch <ahmedomaraali@gmail.com>
2024-03-20 18:14:58 -0400

    anthropic tests for api key

---
+++ tests/unit_tests/models/test_anthropic.py
@@ -0,0 +1,21 @@
+import os
+
+import pytest
+from dotenv import load_dotenv
+
+from proxyllm.provider.anthropic. claude import ClaudeAdapter, AnthropicException
+
+load_dotenv(".env.test")
+
+anthropic_api_key = os.getenv("ANTHROPIC_API_KEY")
+
+def test_empty_api_credentials():
+    with pytest.raises(AnthropicException):
+        ClaudeAdapter(api_key="", auth_token="").get_completion("Sample prompt")
+
+
+def test_invalid_api_credentials():
+    with pytest.raises(AnthropicException):
+        ClaudeAdapter(api_key="invalid_key", auth_token="invalid_token").get_completion("Sample prompt")
+
+
