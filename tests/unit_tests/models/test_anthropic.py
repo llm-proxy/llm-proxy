de8cdac60896b9857d69327a7679617692dfaee5
Glitch <ahmedomaraali@gmail.com>
2024-03-25 20:11:21 -0400

    Changed the prompt message to match other providers

--- tests/unit_tests/models/test_anthropic.py
+++ tests/unit_tests/models/test_anthropic.py
@@ -10,13 +10,24 @@ load_dotenv(".env.test")
 anthropic_api_key = os.getenv("ANTHROPIC_API_KEY")
 
 
-def test_empty_api_credentials():
+def test_empty_api_key():
     with pytest.raises(AnthropicException):
-        chatbot = ClaudeAdapter(api_key="")
-        chatbot.get_completion()
+        adapter = ClaudeAdapter(api_key="sfsfs")
+        adapter.get_completion()
 
 
 def test_invalid_api_credentials():
     with pytest.raises(AnthropicException):
         chatbot = ClaudeAdapter(api_key="invalid_key")
         chatbot.get_completion()
+
+
+def test_claude_tokenize_returns_expected_num_of_input_tokens():
+    # Set up your expected number of tokens based on the 'prompt' string
+    expected_num_of_input_tokens = 10  #
+    max_output_tokens = 256
+    claude = ClaudeAdapter(api_key=anthropic_api_key, max_output_tokens=max_output_tokens, model="claude-3-opus-20240229")
+    prompt = "The quick brown fox jumps over the lazy dog."
+    tokenize_response = claude.tokenize(prompt=prompt)
+
+    assert tokenize_response.num_of_input_tokens == expected_num_of_input_tokens, "The number of input tokens does not match the expected value"
