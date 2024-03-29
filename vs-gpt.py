c904efd849b12c68e90f124d9fbc9f655a6e4bd7
MohamedAhmed74 <moe.ahmed74@gmail.com>
2024-03-29 13:48:42 -0400

    Added call_models

---
+++ vs-gpt.py
@@ -0,0 +1,30 @@
+import os
+import time
+
+from dotenv import load_dotenv
+from proxyllm.provider.openai.chatgpt import OpenAIAdapter
+from proxyllm.proxyllm import LLMProxy
+
+def call_models(prompt: str) -> dict:
+    start_openai = time.perf_counter()
+    openai = OpenAIAdapter(prompt=prompt, model="gpt-4", api_key=OPENAI_API_KEY)
+    openai_output = openai.get_completion(prompt=prompt)
+    end_openai = time.perf_counter()
+    openai_latency = end_openai - start_openai
+
+    start_llmproxy = time.perf_counter()
+    llmproxy = LLMProxy(route_type="cost")
+    llmproxy_output = llmproxy.route(prompt=prompt)
+    end_llmproxy = time.perf_counter()
+    llmproxy_latency = end_llmproxy - start_llmproxy
+
+    return {
+        "openai":{
+            "response":openai_output,
+            "latency":openai_latency
+        },
+        "llmproxy":{
+            "response":llmproxy_output,
+            "latency":llmproxy_latency
+        }
+    }
