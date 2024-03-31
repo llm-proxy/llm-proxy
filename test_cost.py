ebcbb1a05b531597358ec053b77efac79e5de62c
laxitshahi <shahilax@my.yorku.ca>
2024-03-31 19:23:22 -0400

    removed test file

--- test_cost.py
+++ test_cost.py
@@ -8,7 +8,7 @@ from proxyllm import LLMProxy
 def main() -> None:
     # prompt = "I am a man, not a man, but not a man, that is an apple, or a banana!"
     prompt = "what is 1 + 1"
-    prompt = "The quick brown fox jumps over the lazy dog."
+    # prompt = "The quick brown fox jumps over the lazy dog."
     # output = proxy_client.route(route_type="cost", prompt=prompt)
     # print(output)
 
