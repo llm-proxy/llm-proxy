f91d440256764c715e30b94509bd9978e1482330
laxitshahi <shahilax@my.yorku.ca>
2024-04-23 15:54:10 -0400

    added chat_historyfile + refactored proxyllm

--- test_cost.py
+++ test_cost.py
@@ -6,44 +6,15 @@ from proxyllm import LLMProxy
 
 
 def main() -> None:
-    # prompt = "I am a man, not a man, but not a man, that is an apple, or a banana!"
-    # prompt = "The quick brown fox jumps over the lazy dog."
-    # output = proxy_client.route(route_type="cost", prompt=prompt)
-    # print(output)
-    # start = time.time()
-    # end = time.time()
-    # print(f"\nCost route total time taken: {end-start}")
-
     prompt = "what is 1+1"
-    chat_history = [
-        {"role": "system", "content": "you are math bot, reply short and sweet"},
-    ]
     proxy_client = LLMProxy(route_type="cost")
     start = time.time()
-    output = proxy_client.route(prompt=prompt, chat_history=chat_history)
+    output = proxy_client.route(prompt=prompt)
     end = time.time()
-    chat_history = output.chat_history
 
     print("RESPONSE MODEL: ", output.response_model)
     print("RESPONSE: ", output.response)
     print(f"\nCost route total time taken: {end-start}")
-    print("CHAT HISTORY: ", chat_history)
-
-    assert output.response_model
-    assert output.response
-
-    # Round 2... FIGHT!
-
-    prompt2 = "what was the question i just asked u before this?"
-    start = time.time()
-    output = proxy_client.route(prompt=prompt2, chat_history=chat_history)
-    end = time.time()
-    chat_history = output.chat_history
-
-    print("RESPONSE MODEL: ", output.response_model)
-    print("RESPONSE: ", output.response)
-    print(f"\nCost route total time taken: {end-start}")
-    print("CHAT HISTORY: ", chat_history)
 
     assert output.response_model
     assert output.response
